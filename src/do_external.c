9a8cf2dadd1c49dac547bbb81953b193f62a6433
stranbird <meteey@gmail.com>
2013-05-12 10:24:49 +0800

    现在应该执行内部命令没有问题了

--- src/do_external.c
+++ src/do_external.c
@@ -2,28 +2,27 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <errno.h>
+#include <stdio.h>
 
 int do_external(char **argv) {
-	int child_pid = 0;
 	int stat_loc = 0;
 	int i = 0;
-	int error;
 //
-//	puts("---------");
-//	for (i = 0; argv[i] != NULL; i++)
-//		printf("%d %s\n", i, argv[i]);
-//	puts("---------");
+	puts("---------");
+	for (i = 0; argv[i] != NULL; i++)
+		printf("%d %s\n", i, argv[i]);
+	puts("---------");
 
-	// if ((child_pid = fork()) != 0) {
-	// 	// parent process
-	// 	waitpid(child_pid, &stat_loc, 0);
-	// }
-	// else {
-	// 	// child process
-    if (execve(argv[0], argv, 0) == -1) {
-        
-    }
-	// }
+	if (fork() != 0) {
+		// parent process
+		waitpid(-1, &stat_loc, 0);
+	}
+	else {
+		// child process
+	    if (execve(argv[0], argv, 0) == -1) {
+	        puts("execution error");
+	    }
+	}
 
 	return 0;
 }
--- src/main.c
+++ src/main.c
@@ -20,7 +20,8 @@
 #define PIPE_W 1
 #define PIPE_R 0
 
-int o_stdout, o_stdin, fd;
+int o_stdout, o_stdin;
+int fd[2], ofd[2];
 
 void reset() {
     // reset the I/O redirect etc.
@@ -52,6 +53,7 @@ int get_cmd(char ***argv, int *is_pipe) {
     char buf[255];
     char ch;
     int i = 0, j;
+    int rfd;
     
     memset(buf, 0, sizeof(buf));
     
@@ -78,11 +80,11 @@ int get_cmd(char ***argv, int *is_pipe) {
                 if (state != 0) {
                     if (state == 1) {
                         close(0);
-                        fd = open(buf, O_RDONLY);
+                        rfd = open(buf, O_RDONLY);
                     }
                     else if (state == 2) {
                         close(1);
-                        fd = open(buf, O_WRONLY | O_CREAT, 0644);
+                        rfd = open(buf, O_WRONLY | O_CREAT, 0644);
                     }
                     
                     state = 0;
@@ -135,12 +137,18 @@ int isMainProcess(pid_t mpid) {
     return mpid == getpid();
 }
 
+int is_internal(const char *cmd) {
+    return (strcmp(cmd, "pwd") && strcmp(cmd, "cd") && strcmp(cmd, "ls")) == 0;
+}
+
+int is_external(const char *cmd) {
+    return access(cmd, X_OK) != -1;
+}
+
 
 int main() {
 	char **cmd_s = NULL;
     int stat_loc;
-    int fd[2], ofd[2];
-    pid_t pid;
     int is_pipe = 0;
     
 //    const pid_t main_process = getpid();
@@ -156,26 +164,61 @@ int main() {
         
 	while (is_cmd(cmd_s[0], "exit") == 0) {
         
-        if (is_pipe) {
-            pipe(fd);            
-        }
-
-        if ((pid = fork()) != 0) {
+        if (is_internal(cmd_s[0])) {
+            if (ofd[PIPE_R] != -1) {
+                close(ofd[PIPE_W]);
+                close(PIPE_R);
+                dup(ofd[PIPE_R]);
+                close(ofd[PIPE_R]);
+            }
             
-//            if (ofd[PIPE_R] == -1)
-                waitpid(-1, &stat_loc, 0);
-
             if (is_pipe) {
-                ofd[0] = fd[0];
-                ofd[1] = fd[1];
+                pipe(fd);
+                
+                ofd[PIPE_R] = dup(fd[PIPE_R]);
+                ofd[PIPE_W] = dup(fd[PIPE_W]);
+                
+                close(fd[PIPE_R]);
+                close(PIPE_W);
+                dup(fd[PIPE_W]);
+                close(fd[PIPE_W]);
             }
-            else {
+            else
                 ofd[PIPE_R] = -1;
-            }
             
-//            waitpid(-1, &stat_loc, 0);
+            if (is_cmd(cmd_s[0], "pwd"))
+            	do_pwd(cmd_s);
+            else if (is_cmd(cmd_s[0], "cd")) {
+              puts("cd");
+            	do_cd(cmd_s);
+            }
+            else if (is_cmd(cmd_s[0], "ls")) {
+              puts("here");
+            	do_ls(cmd_s);
+            }
         }
-        else {
+        else if (is_external(cmd_s[0])) {
+            do_external(cmd_s);
+        }
+        else
+            printf("%s: not found\n", cmd_s[0]);
+
+//        if ((pid = fork()) != 0) {
+//            
+////            if (ofd[PIPE_R] == -1)
+//                waitpid(-1, &stat_loc, 0);
+//
+//            if (is_pipe) {
+//                ofd[0] = fd[0];
+//                ofd[1] = fd[1];
+//            }
+//            else {
+//                ofd[PIPE_R] = -1;
+//            }
+//            
+////            waitpid(-1, &stat_loc, 0);
+//        }
+//        else {
 //            if (ofd[PIPE_R] != -1) {
 //                close(ofd[PIPE_W]);
 //                close(PIPE_R);
@@ -189,32 +232,41 @@ int main() {
 //                dup(fd[PIPE_W]);
 //                close(fd[PIPE_W]);
 //            }
-
-        	// internal commands
-            if (is_cmd(cmd_s[0], "pwd"))
-            	do_pwd(cmd_s);
-            else if (is_cmd(cmd_s[0], "cd")) {
-                puts("cd");
-            	do_cd(cmd_s);
-            }
-            else if (is_cmd(cmd_s[0], "ls")) {
-                puts("here");
-            	do_ls(cmd_s);
-            }
-            // external commands
-            else if (is_executable(cmd_s[0])) {
-            	do_external(cmd_s);
-            }
-            else {
-                printf("%s: not found\n", cmd_s[0]);
-            }
-            
-            printf("should be here2: %s\n", cmd_s[0]);
-            
-            return 0;
-        }
-                
-        reset();
+//
+//        	// internal commands
+//            if (is_cmd(cmd_s[0], "pwd"))
+//            	do_pwd(cmd_s);
+//            else if (is_cmd(cmd_s[0], "cd")) x{
+//                puts("cd");
+//            	do_cd(cmd_s);
+//            }
+//            else if (is_cmd(cmd_s[0], "ls")) {
+//                puts("here");
+//            	do_ls(cmd_s);
+//            }
+//            // external commands
+//            else if (is_executable(cmd_s[0])) {
+//            	do_external(cmd_s);
+//            }
+//            else {
+//                printf("%s: not found\n", cmd_s[0]);
+//            }
+//            
+//            printf("should be here2: %s\n", cmd_s[0]);
+//            
+//            return 0;
+//        }
+        
+//        if (is_pipe) {
+//            ofd[PIPE_R] = fd[PIPE_R];
+//            ofd[PIPE_W] = fd[PIPE_W];
+//        }
+//        else
+//            ofd[PIPE_R] = -1;
+        
+        if (!is_pipe)
+            reset();
+        
 		get_cmd(&cmd_s, &is_pipe);
 	}
     
